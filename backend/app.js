83c53a2c3c4185d3dac541276fe698d8fd854bd4
junk1ord <rakshit.deshmukh2012@gmail.com>
2022-06-25 12:32:07 +0530

    Configured app.js to run the server

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
-
 const app = express();
+
+app.use(express.json());
+
+// ROUTE IMPORTS
+const product = require("./routes/productRoute");
+app.use("/api/v1", product);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
-//Config
-
+// CONFIG
 dotenv.config({ path: "backend/config/config.env" });
 
+// CONNECT TO DATABASE
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
-  console.log("Server is working on http://localhost:${process.env.PORT}");
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
