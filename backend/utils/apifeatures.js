e26060b763c0285689394a6617a596c46cc5ea6f
TheAlphaQ <kennynihar@gmail.com>
2022-06-26 02:11:56 +0530

    add search, filter, pagination backend apis

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -14,8 +14,35 @@ class ApiFeatures {
         }
       : {};
 
-    console.log(keyword);
     this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    //Remove some fields for category
+    const removeFields = ["keyword", "page", "limit"];
+
+    removeFields.forEach((key) => delete queryCopy[key]);
+
+    //Filter for Price and Rating
+
+    console.log(queryCopy);
+
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    console.log(queryStr);
+    return this;
+  }
+
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+
+    const skip = resultPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resultPerPage).skip(skip);
     return this;
   }
 }
