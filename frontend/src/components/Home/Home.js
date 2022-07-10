18c15d6ca8bba4d76d2a6f37583714e7256facfe
junk1ord <rakshit.deshmukh2012@gmail.com>
2022-07-11 03:16:32 +0530

    Refurbishing Redux Initialisation

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,38 +1,39 @@
 import "./Home.css";
-import { CgMouse } from "react-icons/cg";
 import Product from "./Product.js";
 import MetaData from "../layout/MetaData";
-import React, { useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import Rellax from "rellax";
+import { getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
 
 const product = {
   name: "The Dresden Files",
   images: [{ url: "https://i.imgur.com/broLZqb.jpeg" }],
   price: "150",
-  _id: "abhishek",
+  _id: "dresdenFiles",
 };
 
 const Home = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     new Rellax(".rellax"); // <---- Via class name
   }, []);
+
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
 
   return (
     <div className="homeSection">
       <MetaData title="Home" />
       <div className="banner">
-        <h1 className="rellax" data-rellax-speed="-5">
+        <h1 className="rellax" data-rellax-speed="-1">
           Welcome to CoverPage
         </h1>
-        <h3 className="rellax" data-rellax-speed="5">
+        <h3 className="rellax" data-rellax-speed="-3" data-rellax-zindex="-1">
           The bookstore that reads you while you read them.
         </h3>
-
-        <a href="#homeHeading">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
       </div>
 
       <h2 id="homeHeading" className="homeHeading">
