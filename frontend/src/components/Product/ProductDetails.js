7ec949cdee0cf5dd63820d0320d4848c2ddc7b0c
Rakshit Deshmukh <rakshit.deshmukh2012@gmail.com>
2022-07-15 18:33:06 +0530

    -Developed the Product Details page -Only the Carousel remains buggy -Possible issues with react-material-ui-carousel

--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -4,6 +4,7 @@ import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
 import { useParams } from "react-router-dom";
+import { Rating } from "@mui/material";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -12,6 +13,13 @@ const ProductDetails = () => {
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
   );
+
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
 
   useEffect(() => {
     dispatch(getProductDetails(id));
@@ -30,8 +38,46 @@ const ProductDetails = () => {
                   src={item.url}
                   alt={`${i} Slide`}
                 />
+                // <p>Lund</p>
               ))}
           </Carousel>
+        </div>
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <Rating {...options} />
+            <span className="detailsBlock-2-span">
+              {" "}
+              ({product.numOfReviews} Reviews)
+            </span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input readOnly type="number" value="1" />
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+
+            <p>
+              Status:
+              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+
+          <button className="submitReview">Submit Review</button>
         </div>
       </div>
     </>
