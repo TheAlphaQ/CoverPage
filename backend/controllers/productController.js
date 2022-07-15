10f40df1ee4dfaf2cb5631c65b8259392003a666
Rakshit Deshmukh <rakshit.deshmukh2012@gmail.com>
2022-07-15 17:10:25 +0530

    -Fixed the productController variable names -Added the Carousel for images under ProductDetails component -Check Eragon book for sample

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,7 +16,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 //Get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const resultPerPage = 8;
-  const productCount = await Product.countDocuments();
+  const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
@@ -26,7 +26,7 @@ exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   res.status(200).json({
     success: true,
     products,
-    productCount,
+    productsCount,
   });
 });
 
--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
+
   return (
     <>
       <div className="ProductDetails">
         <div>
-          <Carousel></Carousel>
+          <Carousel>
+            {product.images &&
+              product.images.map((item, i) => (
+                <img
+                  className="CarouselImage"
+                  key={item.url}
+                  src={item.url}
+                  alt={`${i} Slide`}
+                />
+              ))}
+          </Carousel>
         </div>
       </div>
     </>
