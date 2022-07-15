079ce9523a9df3e1572f4cde0fd5a00b9404df63
Rakshit Deshmukh <rakshit.deshmukh2012@gmail.com>
2022-07-16 01:11:31 +0530

    Overrode CSS, removed Rellax & checkpoint -Overrode CSS for overlay-navbar Hamburger icon -Removed Rellax as it won't work with Bootstrap -Save point before uninstalling MaterialUI Carousel

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,7 +2,6 @@ import "./Home.css";
 import Product from "./Product.js";
 import MetaData from "../layout/MetaData";
 import React, { useEffect } from "react";
-import Rellax from "rellax";
 import { getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from "../layout/Loader/Loader";
@@ -14,10 +13,6 @@ const Home = () => {
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
   );
-
-  useEffect(() => {
-    new Rellax(".rellax"); // <---- Via class name
-  }, []);
 
   useEffect(() => {
     if (error) {
@@ -34,16 +29,8 @@ const Home = () => {
         <div className="homeSection">
           <MetaData title="CoverPage" />
           <div className="banner">
-            <h1 className="rellax" data-rellax-speed="-1">
-              Welcome to CoverPage
-            </h1>
-            <h3
-              className="rellax"
-              data-rellax-speed="-3"
-              data-rellax-zindex="-1"
-            >
-              The bookstore that reads you while you read them.
-            </h3>
+            <h1>Welcome to CoverPage</h1>
+            <h3>The bookstore that reads you while you read them.</h3>
           </div>
 
           <h2 id="homeHeading" className="homeHeading">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const options = {
   timeout: 5000,
